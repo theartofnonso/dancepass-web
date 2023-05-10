574b948ec1e7752970393c37b8973b9940775019
theartofnonso <slimbion@gmail.com>
2023-05-10 14:26:00 +0100

    Removed AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -79,24 +79,13 @@ class MyApp extends StatelessWidget {
               fontSize: 10,
             ),
           )),
-      home: const MyHomePage(title: 'Welcome Dancepass Web'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -587,11 +576,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
         backgroundColor: Colors.grey.shade50,
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
         body: Center(
           child: SingleChildScrollView(
             child: Container(
